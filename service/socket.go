a5bb93fc029eafaf60d52d747e7e837dd3860950
ouzhibing <ouzhibing@galaxymx.com>
2020-02-26 10:52:50 +0800

    添加遗漏的文件

---
+++ service/socket.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"fmt"
+	"socket/util"
+	"time"
+)
+
+type GlobalSocket struct {
+	ClientConnMap map[string]*Connection
+}
+
+/**
+ * 客户端连接数
+ */
+func (socket *GlobalSocket) CountClient() int {
+	return len(socket.ClientConnMap)
+}
+
+/*
+ * 检测心跳
+ */
+func (socket *GlobalSocket) Heartbeat()  {
+	for{
+		for _,conn := range socket.ClientConnMap {
+
+			jsonData := util.Json(201,"heartbeat",nil)
+			if err := conn.PushToChan(jsonData);err != nil{
+				//客户端断开连接
+				delete(socket.ClientConnMap,conn.clientId)
+			}
+		}
+
+		time.Sleep(5*time.Second)
+
+		//检测客户端连接数
+		fmt.Printf("客户端连接数：%v\n",socket.CountClient())
+	}
+}
